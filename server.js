4ba98022ca83d0d24f167d42e5885f32550be94a
afrid2004 <mdfaisalafrid@gmail.com>
2026-09-23 20:13:25 +0600

    Add Next.js HostSeba CI/CD deployment

---
+++ server.js
@@ -0,0 +1,16 @@
+const { createServer } = require("http");
+const next = require("next");
+
+const port = parseInt(process.env.PORT || "3000", 10);
+const dev = false;
+
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  createServer((req, res) => {
+    handle(req, res);
+  }).listen(port, () => {
+    console.log(`> Next.js server running on port ${port}`);
+  });
+});
